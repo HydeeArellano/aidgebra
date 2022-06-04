55904cb88781d0a1b8081490584c35a7fd92408e
aughusbueno <aughusf089@gmail.com>
2022-06-05 05:22:42 +0800

    replace commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const app = express()
+const server = require("http").createServer(app)
+const port = process.env.PORT || 3001
+const mongoose = require("mongoose")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(cors({
+    credentials : true
+}))
+app.use(cookieParser())
+
+mongoose.connect(process.env.MONGODB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+const connection = mongoose.connection
+
+app.get("/test",async (req, res) => {
+    try{
+        return res.json({status:true})
+    }
+    catch(error){
+        console.log(error)
+        return res.json({status:true, error})
+    }
+})
+
+server.listen(port, () => console.log(`server runs at ${port}`))
